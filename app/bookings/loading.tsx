4a43e0256920b66b1c39dec5f62162049bf36208
padash00 <76392786+padash00@users.noreply.github.com>
2025-04-29 14:19:44 +0500

    Update loading.tsx

--- app/bookings/loading.tsx
+++ app/bookings/loading.tsx
@@ -1,4 +1,7 @@
 export default function Loading() {
-  return null
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary"></div>
+    </div>
+  )
 }
-
