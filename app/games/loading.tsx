bf2121a8581403d051ce0c0ed6b5e3a95123c029
padash00 <76392786+padash00@users.noreply.github.com>
2025-04-30 23:39:38 +0500

    Update loading.tsx

--- app/games/loading.tsx
+++ app/games/loading.tsx
@@ -1,4 +1,15 @@
-export default function Loading() {
-  return null
-}
+// games/loading.tsx
+"use client";
 
+import { Loader2 } from "lucide-react"; // Импортируем иконку загрузки из lucide-react
+
+export default function Loading() {
+  return (
+    <div className="flex min-h-screen w-full flex-col items-center justify-center bg-muted/40">
+      <Loader2 className="h-12 w-12 animate-spin text-primary" />
+      <p className="mt-4 text-lg font-semibold text-muted-foreground">
+        Загрузка данных...
+      </p>
+    </div>
+  );
+}
